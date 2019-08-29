2fc7b6650c063b7b8fe87f2226f2ee74a4a3fefa
boldlion <boldlion1988@gmail.com>
2019-08-29 17:00:34 +0300

    Initial Commit

---
+++ Draw/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Draw
+//
+//  Created by Daniela Gerdzhikova on 29.08.19.
+//  Copyright © 2019 Bold Lion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
